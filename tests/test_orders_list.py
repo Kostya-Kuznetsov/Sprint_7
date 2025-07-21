120eace008d7d341a42c12ccf7050a710ea11b31
Kostya Kuznetsov <fckswgg@yandex.ru>
2025-07-21 13:00:32 +0300

    добавил проверку ответа

--- tests/test_orders_list.py
+++ tests/test_orders_list.py
@@ -7,4 +7,8 @@ class TestOrderList:
     def test_order_list(self):
 
         response = requests.get(url_create_order)
-        assert response.status_code == 200, f"Ожидаемый статус 200, полученный {response.status_code}"
+        assert response.status_code == 200, f"Ожидаемый статус 200, получен {response.status_code}"
+        json_data = response.json()
+        assert 'orders' in json_data, "В ответе отсутствует ключ 'orders'"
+        assert len(json_data['orders']) > 0, "Список 'orders' пуст"
+        assert 'id' in json_data['orders'][0], "В первом заказе отсутствует ключ 'id'"
